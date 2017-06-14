63d79b14fa2fd57eef8134adce373da5ef284b0f
Chazeon <chenxing.luo@gmail.com>
2017-06-15 03:09:28 +0800

    Corrected AES128 decryption function.

--- Engine/source/LuaLib/CryptoFunc.cpp
+++ Engine/source/LuaLib/CryptoFunc.cpp
@@ -17,7 +17,7 @@ void CRYPTOFunc::addLibrary()
 	addFunction("CRYPTO_decrypt_aes_128_cbc", CRYPTOFunc::AES128_Decrypt_Lua);
 }
 
-bool CRYPTOFunc::AES128_Decrypt(const char* Ciphertext, size_t CipherLength, const char* Key, size_t KeyLength, char* Out, int& OutLength)
+bool CRYPTOFunc::AES128_Decrypt(const char* CipherText, size_t CipherLength, const char* Key, size_t KeyLength, char* Out, int& OutLength)
 {
 	EVP_CIPHER_CTX* evp;
 
@@ -31,10 +31,16 @@ bool CRYPTOFunc::AES128_Decrypt(const char* Ciphertext, size_t CipherLength, con
 		return false;
 	}
 
-	if(EVP_DecryptInit_ex(evp, EVP_aes_128_cbc(), NULL, (const u8*)Key, NULL) == 0)
+	// The first 16 bytes in CyperText is actually initialization vector.
+	// The CipherLength is the actual length of the ciphered text, aka strlen(CipherText) - 16,
+	// so we can use this length directly.
+
+	// In the EVP_DecryptInit_ex the fifth param is IV, so we pass in the CipherText
+	if (EVP_DecryptInit_ex(evp, EVP_aes_128_cbc(), NULL, (const u8*)Key, (const u8*)CipherText) == 0)
 		goto evp_fail;
 
-	if(EVP_DecryptUpdate(evp, (u8*)Out, &OutLength, (const u8*)Ciphertext, CipherLength) == 0)
+	// In the EVP_DecryptUpdate the forth param is ciphered text, so we pass in the CipherText + 16
+	if(EVP_DecryptUpdate(evp, (u8*)Out, &OutLength, (const u8*)CipherText + 16, CipherLength) == 0)
 		goto evp_fail;
 
 	EVP_CIPHER_CTX_free(evp);
@@ -48,7 +54,8 @@ int CRYPTOFunc::AES128_Decrypt_Lua(lua_State* L)
 	char* plaintext = NULL;
 	size_t cipher_length = 0;
 	size_t key_length = 0;
-	int plaintext_len = 0;
+	int plaintext_length = 0;
+	size_t padding_length = 0;
 
 	ciphertext = luaL_checklstring(L, 1, &cipher_length);
 	key = luaL_checklstring(L, 2, &key_length);
@@ -56,10 +63,14 @@ int CRYPTOFunc::AES128_Decrypt_Lua(lua_State* L)
 
 	memset(plaintext, 0, cipher_length + 1);
 
-	if(AES128_Decrypt(ciphertext, cipher_length, key, key_length, plaintext, plaintext_len) == false)
+
+	if(AES128_Decrypt(ciphertext, cipher_length, key, key_length, plaintext, plaintext_length) == false)
 		lua_pushnil(L);
 	else
-		lua_pushlstring(L, plaintext, plaintext_len);
+	{
+		padding_length = *(plaintext + plaintext_length - 1);
+		lua_pushlstring(L, plaintext, plaintext_length - padding_length);
+	}
 
 	delete[] plaintext;
 	return 1;
